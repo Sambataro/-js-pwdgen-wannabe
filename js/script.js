78334ea517bc9fc39cc3c679b37c3387c3ada10e
fortunato <sambataro1990@gmail.com>
2020-11-10 15:06:22 +0100

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// variabile nome utente
+var nameUser;
+nameUser = prompt ("inserisci il tuo nome");
+// variabile nome utente //
+// variabile cognome utente
+var surnameUser;
+surnameUser = prompt ("inserisci il tuo cognome");
+// variabile cognome utente//
+// variabile colore utente
+var colorUser;
+colorUser = prompt ("inserisci il tuo colore preferito");
+// variabile cognome utente//
+// variabile numero passowrod utente
+var numberUser = "19";
+// variabile numero password utente//
+
+document.getElementById('password').innerHTML = nameUser + surnameUser + colorUser + numberUser;
